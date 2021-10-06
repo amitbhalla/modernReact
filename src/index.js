d58f8474106504bc069f24163e7ce5d2a3ef4daf
amitbhalla <amit9815@gmail.com>
2021-10-06 13:24:49 +0530

    Added Card component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import ApprovalCard from './ApprovalCard';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className='ui container comments'>
-            <CommentDetail
-                author='Sam'
-                timeAgo='Today at 4:45 PM'
-                avatar={faker.image.avatar()}
-                comment='lorem ipsum sad diem'
-            />
-            <CommentDetail
-                author='Alex'
-                timeAgo='Today at 2:00 AM'
-                avatar={faker.image.avatar()}
-                comment='lorem ipsum sad diem'
-            />
-            <CommentDetail
-                author='Jane'
-                timeAgo='Yesterday at 4:45 PM'
-                avatar={faker.image.avatar()}
-                comment='lorem ipsum sad diem'
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author='Sam'
+                    timeAgo='Today at 4:45 PM'
+                    avatar={faker.image.avatar()}
+                    comment='lorem ipsum sad diem'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Alex'
+                    timeAgo='Today at 2:00 AM'
+                    avatar={faker.image.avatar()}
+                    comment='lorem ipsum sad diem'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Jane'
+                    timeAgo='Yesterday at 4:45 PM'
+                    avatar={faker.image.avatar()}
+                    comment='lorem ipsum sad diem'
+                />
+            </ApprovalCard>
         </div>
     );
 };
