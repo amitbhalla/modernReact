ef33df8dc9fc35b340b9b0ae604b4a8f1892d7ba
amitbhalla <amit9815@gmail.com>
2021-10-06 18:58:43 +0530

    componentDidMount added

--- src/index.js
+++ src/index.js
@@ -2,42 +2,45 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { latitude: null, errorMessage: '' };
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ latitude: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null, errorMessage: '' };
+  }
 
-    render() {
-        if (this.state.errorMessage && !this.state.latitude) {
-            return (
-                <div>
-                    Error:
-                    <strong>{' ' + this.state.errorMessage}</strong>
-                </div>
-            );
-        } else if (!this.state.errorMessage && this.state.latitude) {
-            return (
-                <div>
-                    Latitude:
-                    <strong>{' ' + this.state.latitude}</strong>
-                </div>
-            );
-        } else {
-            return <div>Loading...</div>;
-        }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return (
+        <div>
+          Error:
+          <strong>{' ' + this.state.errorMessage}</strong>
+        </div>
+      );
+    } else if (!this.state.errorMessage && this.state.latitude) {
+      return (
+        <div>
+          Latitude:
+          <strong>{' ' + this.state.latitude}</strong>
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>;
     }
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
