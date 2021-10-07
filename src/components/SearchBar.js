4ab502385b7fc6186782edfcd0c7338311e719ef
amitbhalla <amit9815@gmail.com>
2021-10-07 20:36:03 +0530

    Search component updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,20 +3,36 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+  state = { term: '' };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Make sure to callback from App component
+  };
+
   render() {
     return (
       <div className='search-container'>
-        <div className='search-content'>
-          <label htmlFor='search' className='label'>
-            Video Search
-          </label>
-          <input
-            placeholder='Type a video name...'
-            type='text'
-            className='search-bar'
-            id='search'
-          />
-        </div>
+        <form onSubmit={this.onFormSubmit}>
+          <div className='search-content'>
+            <label htmlFor='search' className='label'>
+              Video Search
+            </label>
+            <input
+              placeholder='Type a video name...'
+              type='text'
+              className='search-bar'
+              id='search'
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
