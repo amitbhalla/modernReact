56c20635a040a9b0252056b7e5db5229ca2de841
amitbhalla <amit9815@gmail.com>
2021-10-09 12:45:27 +0530

    Base functionality added on dropdown

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import './App.css';
-// import Accordion from './components/Accordion';
-import Search from './components/Search';
+import { useState } from 'react';
 
-const items = [
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
+import './App.css';
+
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content:
+//       'React is a JavaScript library for building user interfaces. Learn what React is all about on our homepage or in the tutorial.',
+//   },
+//   {
+//     title: 'How to try React?',
+//     content:
+//       'React has been designed from the start for gradual adoption, and you can use as little or as much React as you need.',
+//   },
+//   {
+//     title: 'How to add React to a website?',
+//     content:
+//       'You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.',
+//   },
+// ];
+
+const options = [
   {
-    title: 'What is React?',
-    content:
-      'React is a JavaScript library for building user interfaces. Learn what React is all about on our homepage or in the tutorial.',
+    label: 'Red',
+    value: 'red',
   },
   {
-    title: 'How to try React?',
-    content:
-      'React has been designed from the start for gradual adoption, and you can use as little or as much React as you need.',
+    label: 'Blue',
+    value: 'blue',
   },
   {
-    title: 'How to add React to a website?',
-    content:
-      'You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.',
+    label: 'Green',
+    value: 'green',
   },
 ];
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className='App'>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 }
