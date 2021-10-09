c59efd70ac65a45ce9b70eb30f6c9281f87eb9a0
amitbhalla <amit9815@gmail.com>
2021-10-09 14:55:00 +0530

    widgets archived

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -5,25 +5,25 @@ import { useState } from 'react';
 import Dropdown from './components/Dropdown';
 
 import './App.css';
-
-// const items = [
-//   {
-//     title: 'What is React?',
-//     content:
-//       'React is a JavaScript library for building user interfaces. Learn what React is all about on our homepage or in the tutorial.',
-//   },
-//   {
-//     title: 'How to try React?',
-//     content:
-//       'React has been designed from the start for gradual adoption, and you can use as little or as much React as you need.',
-//   },
-//   {
-//     title: 'How to add React to a website?',
-//     content:
-//       'You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.',
-//   },
-// ];
-
+/*
+const items = [
+  {
+    title: 'What is React?',
+    content:
+      'React is a JavaScript library for building user interfaces. Learn what React is all about on our homepage or in the tutorial.',
+  },
+  {
+    title: 'How to try React?',
+    content:
+      'React has been designed from the start for gradual adoption, and you can use as little or as much React as you need.',
+  },
+  {
+    title: 'How to add React to a website?',
+    content:
+      'You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.',
+  },
+];
+*/
 const options = [
   {
     label: 'Red',
